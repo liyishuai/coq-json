072f2c866bed7d47b5a470180d58f07c9f48b011
Yishuai Li <yishuai@upenn.edu>
2021-05-03 00:10:46 -0400

    JSON to Sexp

---
+++ Instances.v
@@ -0,0 +1,22 @@
+From ExtLib Require Export
+     Extras.
+From Ceres Require Export
+     Ceres.
+From JSON Require Export
+     JSON.
+Export
+  FunNotation.
+Open Scope sexp_scope.
+
+Instance Serialize__json : Serialize json :=
+  fix to_sexp (j : json) : sexp :=
+    match j with
+    | JSON__Object l => List $ map (fun sj => let (s, j) := sj : string * json in
+                                        [Atom s; to_sexp j]) l
+    | JSON__Array  l => List $ map to_sexp l
+    | JSON__String s => Atom s
+    | JSON__Number z => Atom z
+    | JSON__True     => Atom "true"
+    | JSON__False    => Atom "false"
+    | JSON__Null     => Atom "null"
+    end.
