0da4cdf7449b9679588adea45adb3c6720a6bd4d
Yishuai Li <liyishuai.lys@alibaba-inc.com>
2023-09-04 17:44:00 +0800

    Operator: fix deprecation command

--- Operator.v
+++ Operator.v
@@ -20,14 +20,14 @@ Definition t : Set := string * json.
 Definition _ascii_compare (a b : ascii) : comparison :=
   N.compare (N_of_ascii a) (N_of_ascii b).
 
-#[deprecated(since="8.14", note="Use Ascii.compare instead.")]
+#[deprecated(since="8.15", note="Use Ascii.compare instead.")]
  Notation ascii_compare := _ascii_compare (only parsing).
 
 Lemma _ascii_compare_antisym (a b : ascii) :
     ascii_compare a b = CompOpp (ascii_compare b a).
 Proof. apply N.compare_antisym. Qed.
 
-#[deprecated(since="8.14", note="Use Ascii.compare_antisym instead.")]
+#[deprecated(since="8.15", note="Use Ascii.compare_antisym instead.")]
  Notation ascii_compare_antisym := _ascii_compare_antisym (only parsing).
 
 Fixpoint _string_compare (s1 s2 : string) : comparison :=
@@ -42,7 +42,7 @@ Fixpoint _string_compare (s1 s2 : string) : comparison :=
     end
   end.
 
-#[deprecated(since="8.14", note="Use String.compare instead.")]
+#[deprecated(since="8.15", note="Use String.compare instead.")]
  Notation string_compare := _string_compare (only parsing).
 
 Lemma _string_compare_antisym : forall s1 s2 : string,
@@ -54,13 +54,13 @@ Proof.
   destruct (ascii_compare a0 a); simpl; intuition.
 Qed.
 
-#[deprecated(since="8.14", note="Use String.compare_antisym instead.")]
+#[deprecated(since="8.15", note="Use String.compare_antisym instead.")]
  Notation string_compare_antisym := _string_compare_antisym (only parsing).
 
 Definition _string_leb (s1 s2 : string) : bool :=
   if string_compare s1 s2 is Gt then false else true.
 
-#[deprecated(since="8.14", note="Use String.leb instead.")]
+#[deprecated(since="8.15", note="Use String.leb instead.")]
  Notation string_leb := _string_leb (only parsing).
 
 Lemma _string_leb_total (s1 s2 : string) :
@@ -71,7 +71,7 @@ Proof.
   destruct (string_compare s2 s1); intuition.
 Qed.
 
-#[deprecated(since="8.14", note="Use String.leb_total instead.")]
+#[deprecated(since="8.15", note="Use String.leb_total instead.")]
  Notation string_leb_total := _string_leb_total (only parsing).
 
 Definition leb (x y : t) : bool :=
